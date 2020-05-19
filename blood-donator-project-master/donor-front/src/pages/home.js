4fa9fa4667fd8f672161484320792e450b852d37
Richard Beletatti <richard.beletatti@gmail.com>
2020-05-19 16:02:29 -0300

    Quem somos e Apoiadores criado

---
+++ blood-donator-project-master/donor-front/src/pages/home.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+    render() {
+        return (
+            <center>
+            <div className = 'home'>
+                <h4>Blood Donator Project</h4>
+                <div className = 'center'>
+
+                <p>Aqui será a tela Inicial</p>
+
+                </div>
+            </div>
+            </center>
+        )
+   }
+};
+
+export default Home;
